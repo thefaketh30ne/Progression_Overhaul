113e664e97410ce3e0365c99b113e979b85ad158
thefaketh30ne <110573217+thefaketh30ne@users.noreply.github.com>
2022-08-04 21:07:47 +0100

    Initialised 'ModBlocks' class

--- src/main/java/com/th30ne/progression_overhaul/block/ModBlocks.java
+++ src/main/java/com/th30ne/progression_overhaul/block/ModBlocks.java
@@ -11,9 +11,17 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, progression_overhaul.MOD_ID);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
 
